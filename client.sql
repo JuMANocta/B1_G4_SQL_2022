36c6ac527f6586db3cf70dfb8044fd88c7594268
dev <dev@dev.dev>
2022-11-24 11:29:28 +0100

    ajout alter table

--- client.sql
+++ client.sql
@@ -82,3 +82,36 @@ SELECT ville FROM client;-- 10 lignes
 SELECT DISTINCT ville FROM client;-- 5 lignes
 SELECT ville FROM client GROUP BY ville;-- 5 lignes
 -- afficher les nom et prenom des personnes qui habitent à Paris
+SELECT nom, prenom FROM client WHERE ville = 'Paris';
+
+-- afficher les nom et prenom des personnes qui habitent à Paris ou à Lyon
+SELECT nom, prenom FROM client WHERE ville = 'Paris' OR ville = 'Lyon';
+-- afficher le nom et prenom des personnes dont le nom fini par 'd'
+SELECT nom, prenom FROM client WHERE nom LIKE '%d';
+-- afficher toutes les personnes qui habitent à Paris ou à Lyon ou grenoble
+SELECT nom, prenom FROM client WHERE ville IN ('Paris', 'Lyon', 'Grenoble');
+-- afficher toutes les personnes comprisent en tre l'id 1 et 3
+SELECT nom, prenom FROM client WHERE identifiant BETWEEN 1 AND 3;
+
+-- ajouter une colonne age dans la table client
+ALTER TABLE client ADD age INT;
+DROP TABLE IF EXISTS client;
+CREATE TABLE client (
+    identifiant INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    prenom VARCHAR(25) NOT NULL,
+    nom VARCHAR(25) NOT NULL,
+    ville VARCHAR(25) NOT NULL,
+    age INT NOT NULL
+);
+INSERT INTO client (prenom, nom, ville, age) 
+VALUES 
+('Pierre', 'Dupont', 'Paris', 25),
+('Sabrina', 'Durand', 'Nantes', 30),
+('Julien', 'Martin', 'Lyon', 35),
+('David', 'Bernard', 'Marseille', 40),
+('Marie', 'Leroy', 'Grenoble', 45),
+('Julien', 'Dupont', 'Paris', 50),
+('Paul', 'Durand', 'Nantes', 55),
+('Macéo', 'Martin', 'Lyon', 60),
+('Maxime', 'Bernard', 'Marseille', 65),
+('Tom', 'Leroy', 'Grenoble', 70);
